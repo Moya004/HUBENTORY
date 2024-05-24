c97aac8bed896ac9b544f458db092363cca96f21
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-23 19:58:59 -0500

    Vista de productos

---
+++ FRONTEND/Front_Hubentory/src/routesViews/Products.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../layout/navBar";
+
+export default function  Products(){
+    return (
+        
+        <NavBar>
+            <h1>vista de Products</h1>
+        </NavBar>
+
+    );
+}
