2fc0c3b18087ecfd3515fa9028b00bc17a7a0a83
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-23 20:00:18 -0500

    Creacion de vista informes

---
+++ FRONTEND/Front_Hubentory/src/routesViews/Reports.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../layout/navBar";
+
+export default function  Reports(){
+    return (
+        <>
+            <NavBar>
+                <h1>vista de informes</h1>
+            </NavBar>
+        </>
+
+    );
+}
