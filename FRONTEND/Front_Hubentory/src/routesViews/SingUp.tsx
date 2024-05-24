d19ed9e6f2a6b87485778036616b8a4ece7e7020
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-23 19:59:30 -0500

    Creación de vista de registro

--- FRONTEND/Front_Hubentory/src/routesViews/SingUp.tsx
+++ FRONTEND/Front_Hubentory/src/routesViews/SingUp.tsx
@@ -1,3 +1,33 @@
-export default function singUp(){
-    return <h1>Sing up</h1>;
+import './singUp.css'; 
+import React from 'react';
+const singUp:React.FC = () =>{
+    return(
+        <div>
+            <form className="singup-form">
+                <h1>Sing Up</h1>
+                <label htmlFor="id">Id:</label>
+                <input type="text" id="id" />
+
+                <label htmlFor="name">Nombre Completo:</label>
+                <input type="text" id="name" />
+
+                <label htmlFor="password">Contraseña:</label>
+                <input type="password" id="password" />
+
+                <label htmlFor="verify_password">Confirmar Contraseña:</label>
+                <input type="password" id="verify_password" />
+
+                <label htmlFor="id_inv">Id de inventario:</label>
+                <input type="text" id="id" />
+
+                <label htmlFor="name_inv">Nombre de inventario:</label>
+                <input type="text" id="name" />
+
+                <button type="submit">Registrarte</button>
+
+            </form>
+        </div>
+    );
 }
+
+export default singUp;
