ddeb93ac46f7605bb096e9c76d6c84f0a231dcea
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-24 08:47:01 -0500

    Añadido logo

--- FRONTEND/Front_Hubentory/src/routesViews/Products.tsx
+++ FRONTEND/Front_Hubentory/src/routesViews/Products.tsx
@@ -1,11 +1,21 @@
-import NavBar from "../layout/navBar";
+import NavBar from "../Components/navBar";
+import DataTable from '../Components/tableProducts';
+import logo from '../assets/logo.png';
+import './stylesproducts.css';
 
-export default function  Products(){
+
+export default function Products() {
     return (
-        
-        <NavBar>
-            <h1>vista de Products</h1>
-        </NavBar>
-
+        <div className="container">
+            <NavBar>
+                <img className="logo-hub" src={logo}></img>
+            </NavBar>
+            <div>
+                <div className="content">
+                    <h1>Productos</h1>
+                </div>
+                <DataTable />
+            </div>
+        </div>
     );
 }
