8336c4ecd8b449458a10be6c0767e4b6c3d0371d
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-23 19:56:00 -0500

    Autenticación de acceso

--- FRONTEND/Front_Hubentory/src/Auth/AuthProvider.tsx
+++ FRONTEND/Front_Hubentory/src/Auth/AuthProvider.tsx
@@ -12,7 +12,7 @@ export function AuthProvider({children}: AuthProviderProps){
 
     // autentificacion para que pase a la informacion protegida enviada del backend al fronted
 
-    const [isAuthenticated, setIsAuthenticated] = useState(false); //donde se encuentran estos valores de verificacion se cambian por las variables creadas por mario del backend
+    const [isAuthenticated, setIsAuthenticated] = useState(true); //donde se encuentran estos valores de verificacion se cambian por las variables creadas por mario del backend
 
     return (<AuthContext.Provider value={{isAuthenticated}}>{children}</AuthContext.Provider>);
 }
