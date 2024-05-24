e83be799ee35e6ce2d09eb0c1fdc1ff9db94a45d
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-23 19:54:22 -0500

    Protección de rutas

--- FRONTEND/Front_Hubentory/src/routesViews/ProtectedRoutes.tsx
+++ FRONTEND/Front_Hubentory/src/routesViews/ProtectedRoutes.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
 import { Outlet ,  Navigate } from "react-router-dom";
 import { useAuth } from "../Auth/AuthProvider";
 export default function protectedroutes(){
     const auth = useAuth()
+    
     return auth.isAuthenticated ? <Outlet /> : <Navigate to="/" />
 
 }
