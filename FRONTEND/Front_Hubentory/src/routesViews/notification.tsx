7cf075548d83890bf80f0d68b25bf2864e300644
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-25 18:08:30 -0500

    Nueva vista

---
+++ FRONTEND/Front_Hubentory/src/routesViews/notification.tsx
@@ -0,0 +1,16 @@
+import './stylesLogin.css'; 
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import img_logo from '../assets/img_logo.png';
+const Notification:React.FC = () => {
+
+    return (
+        
+        <div className='containerNoti'>
+                <h1>Notification</h1>
+        </div>
+    );    
+}
+
+// revisa no entinedo porque me genera errores honestamente
+export default Notification;
