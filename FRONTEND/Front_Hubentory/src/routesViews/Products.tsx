603a7e9c9c296bbdff724198f54d37077c999f42
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-25 07:38:58 -0500

    Importación de recursos

--- FRONTEND/Front_Hubentory/src/routesViews/Products.tsx
+++ FRONTEND/Front_Hubentory/src/routesViews/Products.tsx
@@ -1,6 +1,9 @@
 import NavBar from "../Components/navBar";
 import DataTable from '../Components/tableProducts';
 import logo from '../assets/logo.png';
+import img_left from '../assets/left.png';
+import img_right from '../assets/right.png';
+import btn_alerts from '../assets/alerts.png';
 import { useNavigate } from 'react-router-dom';
 import './stylesproducts.css';
 import '../Components/styleHeaded.css';
@@ -36,15 +39,23 @@ export default function Products() {
     return (
         <div className="container">
             <NavBar>
-                <img className="logo-hub" src={logo} alt="Logo"></img>
+                <img className="logo-hub" src={logo} alt="Logo" />
             </NavBar>
             <div>
                 <div className="content">
-                  <div className="header">
-                    <button onClick={handlePreviousClick}>Vista Anterior</button>
-                    <button onClick={handleNextClick}>Vista Siguiente</button>
-                  </div>
+                    <div className="header">
+                        <button className='btn-previous' onClick={handlePreviousClick}>
+                            <img src={img_left} alt="Previous" />
+                        </button>
+                        <button className='btn-next' onClick={handleNextClick}>
+                            <img src={img_right} alt="Next" />
+                        </button>
+                        <button className='btn-alerts'>
+                            <img src={btn_alerts} alt="Alerts" />
+                        </button>
+                    </div>
                     <h1>Productos</h1>
+                    <p>Todos los productos registrados actualmente en el inventario</p>
                 </div>
                 <DataTable />
             </div>
