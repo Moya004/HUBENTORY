2e575359e67df8d3585cfb646883197ff0202fdc
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-24 21:38:29 -0500

    Navegacion de vistas

--- FRONTEND/Front_Hubentory/src/routesViews/Products.tsx
+++ FRONTEND/Front_Hubentory/src/routesViews/Products.tsx
@@ -1,17 +1,49 @@
 import NavBar from "../Components/navBar";
 import DataTable from '../Components/tableProducts';
 import logo from '../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 import './stylesproducts.css';
+import '../Components/styleHeaded.css';
 
+
+// export default function Products() {
+//     return (
+//         <div className="container">
+//             <NavBar>
+//                 <img className="logo-hub" src={logo}></img>
+//             </NavBar>
+//             <div>
+//                 <div className="content">
+//                     <h1>Productos</h1>
+//                 </div>
+//                 <DataTable />
+//             </div>
+//         </div>
+//     );
+// }
 
 export default function Products() {
+    const navigate = useNavigate();
+
+    const handlePreviousClick = () => {
+        navigate(-1); // Navegar a la vista anterior
+    };
+
+    const handleNextClick = () => {
+        navigate(1); // Navegar a la vista siguiente
+    };
+
     return (
         <div className="container">
             <NavBar>
-                <img className="logo-hub" src={logo}></img>
+                <img className="logo-hub" src={logo} alt="Logo"></img>
             </NavBar>
             <div>
                 <div className="content">
+                  <div className="header">
+                    <button onClick={handlePreviousClick}>Vista Anterior</button>
+                    <button onClick={handleNextClick}>Vista Siguiente</button>
+                  </div>
                     <h1>Productos</h1>
                 </div>
                 <DataTable />
--- FRONTEND/Front_Hubentory/src/routesViews/Reports.tsx
+++ FRONTEND/Front_Hubentory/src/routesViews/Reports.tsx
@@ -1,14 +1,35 @@
 import NavBar from "../Components/navBar";
 import logo from '../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 import './stylesReports.css';
+import '../Components/styleHeaded.css';
 
 export default function  Reports(){
-    return (
-        <>
-            <NavBar>
-                <img className="logo-hub" src={logo}></img>
-            </NavBar>
-        </>
+    const navigate = useNavigate();
 
+    const handlePreviousClick = () => {
+        navigate(-1); // Navegar a la vista anterior
+    };
+
+    const handleNextClick = () => {
+        navigate(1); // Navegar a la vista siguiente
+    };
+
+    return (
+        <div className="container">
+            <NavBar>
+                <img className="logo-hub" src={logo} alt="Logo"></img>
+            </NavBar>
+            <div>
+                <div className="content">
+                  <div className="header">
+                    <button onClick={handlePreviousClick}>Vista Anterior</button>
+                    <button onClick={handleNextClick}>Vista Siguiente</button>
+                  </div>
+                    <h1>Lista de informes</h1>
+                </div>
+                
+            </div>
+        </div>
     );
 }
