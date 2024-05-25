7d082a00a27372f56aa3f04ef3e52855e90ea230
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-25 14:20:33 -0500

    Actualizacion de contenedores

--- FRONTEND/Front_Hubentory/src/routesViews/Login.tsx
+++ FRONTEND/Front_Hubentory/src/routesViews/Login.tsx
@@ -1,10 +1,21 @@
-import './Login.css'; 
+import './stylesLogin.css'; 
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import img_logo from '../assets/img_logo.png';
 const Login:React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleCustomPathClick = () => {
+        navigate('/singUp'); 
+    };
+
+
     return (
-        <div>
+        
+        <div className='containerlogin'>
+            <div className='background-rectangle'></div>
             <form className="login-form">
+                <img src={img_logo} />
                 <h1>Login</h1>
                 <label htmlFor="id">id</label>
                 <input type="text" id="id" />
@@ -14,7 +25,7 @@ const Login:React.FC = () => {
 
                 <button type="submit">Iniciar sesion</button>
 
-                <button type="submit" className='singup'>Registrarse</button>
+                <button type="submit" className='singup' onClick={handleCustomPathClick}>Registrarse</button>
             </form>
         </div>
     );    
