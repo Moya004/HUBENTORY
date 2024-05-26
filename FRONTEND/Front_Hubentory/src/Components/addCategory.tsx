d74ed9001ca8cf19675efe18b4167bb701f9fa98
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-25 22:45:29 -0500

    añadi campos de form

--- FRONTEND/Front_Hubentory/src/Components/addCategory.tsx
+++ FRONTEND/Front_Hubentory/src/Components/addCategory.tsx
@@ -20,7 +20,7 @@ const AddCategory: React.FC<AddCategoryProps> = ({ isOpen, onClose, children }) 
                 {children}
                 <form className="category-form">
                     <input className="id_cat" type="text" id="id_category" placeholder="   ID Categoria"/>
-                </form>
+</form>
                 <div className='container-btnadd'>
                     <button className='btnCancel' onClick={onClose}>Cancelar</button>
                     <button className='btnadd'>Generar</button>
