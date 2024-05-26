9b37fd96bc36ef72875feebbd05793401f68ab74
Viviana Prasca <vivianaprascamendez@gmail.com>
2024-05-26 11:46:28 -0500

    Creacion de elementos de vista

--- FRONTEND/Front_Hubentory/src/Components/removal.tsx
+++ FRONTEND/Front_Hubentory/src/Components/removal.tsx
@@ -12,18 +12,16 @@ const removal: React.FC<removalProps> = ({ isOpen, onClose, children }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="modal-overlay">
-            <div className="modal-content">
-                <button className="modal-close" onClick={onClose}>
+        <div className="Removal-overlay">
+            <div className="Removal-content">
+                <button className="Removal-close" onClick={onClose}>
                     <img src={close} alt="Close" />
                 </button>
                 {children}
-                <form className="category-form">
-                    <input className="id_cat" type="text" id="id_category" placeholder="   ID Categoria"/>
-</form>
-                <div className='container-btnadd'>
-                    <button className='btnCancel' onClick={onClose}>Cancelar</button>
-                    <button className='btnadd'>Retirar</button>
+                <p>¿Estás seguro de esta acción?</p>
+                <div className='container-removal'>
+                    <button className='btnCancel_Re' onClick={onClose}>No</button>
+                    <button className='btnRemoval'>Si, retirar</button>
                 </div>
             </div>
         </div>
